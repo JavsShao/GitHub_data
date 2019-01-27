1fdaba236957682ef469fca0795418dc26723d97
hamdi <javs_shao@163.com>
2019-01-27 16:37:10 +0800

    Pygal可视化仓库

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,7 @@
 import requests
+import pygal
+
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 
 # 执行API调用并存储响应
@@ -10,19 +13,18 @@ print("Status code:", r.status_code)
 response_dict = r.json()
 print("Total resposerise:", response_dict['total_count'])   # 指出github包含多少个Python仓库
 # 探索有关仓库的信息
-repo_dicts = response_dict['items'] # respose_dict字典存储在列表中, 列表里每个字典都包含有关一个Python仓库的信息
-print("Repositorise returned:", len(repo_dicts))
+repo_dicts = response_dict['items']
+print("Repositorise returned:", len(repo_dicts))        #response_dict字典存储在列表中，列表里每个字典都包含有关一个Python仓库的信息
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
 
-# 研究第一个仓库
-repo_dict = repo_dicts[0]
-print("\nkeys:", len(repo_dicts))
-for key in sorted(repo_dict.keys()):
-    print(key)
+#可视化
+my_style = LS('#333366',base_style=LCS)
+chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)#创建条形图，让标签绕x轴旋转45度（x_label_rotation=45 ），隐藏图例（show_legend=False ）
+chart.title = 'Most-Starred Python Projects on GitHub'#指定标题
+chart.x_labels=names#横坐标标签
+chart.add('',stars)#添加数据，标签设置成空字符串
+chart.render_to_file('python_repos.svg')
 
-print("\nSelected information about first repository:")
-for repo_dicts in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Description:', repo_dict['description'])
